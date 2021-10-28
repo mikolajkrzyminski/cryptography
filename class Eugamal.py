8f79574dbea49ee90eb392aeff0aa480d70b74a3
s444417 <mikkrz1@st.amu.edu.pl>
2021-10-28 16:38:34 +0200

    elgamal pseudocode

---
+++ class Eugamal.py
@@ -0,0 +1,23 @@
+class Eugamal:
+
+# algorytm
+# szyfrowanie
+# BOB (p,g,y) 1<m<p-1
+# 1) los 1 < k < p
+# 2) obl c1 = g^k(mod p)
+# 3) c2 = m * y^k(mod p)
+
+# Generowanie klucza
+# p = 2*q + 1
+# Alice
+# 1) los p - l. pierwsza
+# 2) znajdujemy g: <q> = Fi(p)
+# 3) los 1 < x < p - 1
+# 4) obl y = g^x(mod p)
+# 5) Ka = (p, q, y) - publ
+# 6) ka = (p, x)
+
+
+    @staticmethod
+    def decript(self):
+        pass
