60c8f5cee9ed12f40b89901040b269fc38246a6c
s444417 <mikkrz1@st.amu.edu.pl>
2021-11-04 15:13:55 +0100

    rsa fix

--- RSA/RSA.py
+++ RSA/RSA.py
@@ -8,18 +8,18 @@ class RSA:
     def rsa():
         p, q = 0, 0
         while True:
-            p = FermatTest.getRandomNBitsNumber(216)
+            p = FermatTest.getRandomNBitsNumber(256)
             if FermatTest.FermatTest(50, p): break
         while True:
-            q = FermatTest.getRandomNBitsNumber(216)
-            if FermatTest.FermatTest(50, p) and p != q: break
+            q = FermatTest.getRandomNBitsNumber(256)
+            if FermatTest.FermatTest(50, q) and p != q: break
         n = p * q
         fi = (p - 1) * (q - 1)
         e = 0
         while True:
             e = FermatTest.getRandomNBitsNumber(fi.bit_length() - 1)
-            if 1 == Euclides.extendEuclides(fi, e)[2]: break
-        d = Euclides.getInv(fi, e)
+            if 1 == Euclides.extendEuclides(e, fi)[2]: break
+        d = Euclides.getInv(e, fi)
         pu = open("RSA\public.txt", "w")
         pu.write(str(n) + " " + str(e))
         pu.close()
@@ -34,7 +34,8 @@ class RSA:
         pu.close()
         n = int(public[0])
         e = int(public[1])
-        M = FermatTest.getRandomNBitsNumber(n.bit_length() - 1)
+        #M = FermatTest.getRandomNBitsNumber(n.bit_length() - 1)
+        M = 12345
         print(M)
         C = PowBin.powBinMod(M, e, n)
         return C
